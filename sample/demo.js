7ea4bb96ec51995b15e889f260ed771fe9d00f30
Tsubasa-Kajinuma <Tsubasa_Kajinuma@voyagegroup.com>
2014-12-13 14:57:55 +0900

    [clean] ログの項目を削除

--- sample/demo.js
+++ sample/demo.js
@@ -3,26 +3,17 @@ $(function(){
 	$('.roulette').find('img').hover(function(){
 		console.log($(this).height());
 	});
-	var appendLogMsg = function(msg) {
-		$('#msg')
-	.append('<p class="muted">' + msg + '</p>')
-	.scrollTop(100000000);
-
-	}
 	var p = {
 		startCallback : function() {
-			appendLogMsg('start');
 			$('#speed, #duration').slider('disable');
 			$('#stopImageNumber').spinner('disable');
 			$('.start').attr('disabled', 'true');
 			$('.stop').removeAttr('disabled');
 		},
 		slowDownCallback : function() {
-			appendLogMsg('slowdown');
 			$('.stop').attr('disabled', 'true');
 		},
 		stopCallback : function($stopElm) {
-			appendLogMsg('stop');
 			$('#speed, #duration').slider('enable');
 			$('#stopImageNumber').spinner('enable');
 			$('.start').removeAttr('disabled');
