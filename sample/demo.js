912ca9e23d2e99f394984811988ca2f45d478ecb
Tsubasa-Kajinuma <Tsubasa_Kajinuma@voyagegroup.com>
2014-12-13 16:21:06 +0900

    [clean] 不要なパラメータを削除した

--- sample/demo.js
+++ sample/demo.js
@@ -6,8 +6,6 @@ $(function(){
         speed: 1000,
         duration: 30,
 		startCallback : function() {
-			$('#speed, #duration').slider('disable');
-			$('#stopImageNumber').spinner('disable');
 			$('.start').attr('disabled', 'true');
 			$('.stop').removeAttr('disabled');
 		},
@@ -15,8 +13,6 @@ $(function(){
 			$('.stop').attr('disabled', 'true');
 		},
 		stopCallback : function($stopElm) {
-			$('#speed, #duration').slider('enable');
-			$('#stopImageNumber').spinner('enable');
 			$('.start').removeAttr('disabled');
 			$('.stop').attr('disabled', 'true');
 		}
@@ -25,10 +21,6 @@ $(function(){
 	var rouletter = $('div.roulette');
 	rouletter.roulette(p);	
 	$('.stop').click(function(){
-		var stopImageNumber = $('.stopImageNumber').val();
-		if(stopImageNumber == "") {
-			stopImageNumber = null;
-		}
 		rouletter.roulette('stop');	
 	});
 	$('.stop').attr('disabled', 'true');
@@ -38,65 +30,8 @@ $(function(){
 	});
 
 	var updateParamater = function(){
-		p['stopImageNumber'] = RouletteGift.draw();
+		p.stopImageNumber = RouletteGift.draw();
 		rouletter.roulette('option', p);	
 	}
-	var updateSpeed = function(speed){
-		$('.speed_param').text(speed);
-	}
-	$('#speed').slider({
-		min: 1,
-		max: 30,
-		value : 10,
-		slide: function( event, ui ) {
-			updateSpeed(ui.value);
-		}
-	});
-	updateSpeed($('#speed').slider('value'));
-
-	var updateDuration = function(duration){
-		$('.duration_param').text(duration);
-	}
-	$('#duration').slider({
-		min: 2,
-		max: 10,
-		value : 3,
-		slide: function( event, ui ) {
-			updateDuration(ui.value);
-		}
-	});
-	updateDuration($('#duration').slider('value'));
-
-	var updateStopImageNumber = function(stopImageNumber) {
-		$('.image_sample').children().css('opacity' , 0.2);
-		$('.image_sample').children().filter('[data-value="' + stopImageNumber + '"]').css('opacity' , 1);
-		$('.stop_image_number_param').text(stopImageNumber);
-	}
-
-	$('#stopImageNumber').spinner({
-		spin: function( event, ui ) {
-			var imageNumber = ui.value;
-			if ( ui.value > 4 ) {
-				$( this ).spinner( "value", -1 );
-				imageNumber = 0;	
-				updateStopImageNumber(-1);		
-				return false;
-			} else if ( ui.value < -1 ) {
-				$( this ).spinner( "value", 4 );
-				imageNumber = 4;	
-				updateStopImageNumber(4);		
-				return false;
-			}
-			updateStopImageNumber(imageNumber);		
-		}
-	});
-	$('#stopImageNumber').spinner('value', 0);
-	updateStopImageNumber($('#stopImageNumber').spinner('value'));		
-
-	$('.image_sample').children().click(function(){
-		var stopImageNumber = $(this).attr('data-value');
-		$('#stopImageNumber').spinner('value', stopImageNumber);
-		updateStopImageNumber(stopImageNumber);
-	});
 });
 
